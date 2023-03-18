93e19f9e9c04e588272fa0106f79c68f4823f6dd
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-03-17 20:08:52 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0, y;
+
+	while (x <= 99)
+	{
+		y = x;
+		while (y <= 99)
+
+			if (y != x)
+			{
+				putchar((x / 10) + 48;
+				putchar((x % 10) + 48;
+				putchar(' ');
+				putchar((y / 10) + 48;
+				putchar((y / 10) + 48;
+
+					if (x != 98 || y != 99)
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+					y++
+					}
+					x++
+					}
+					patcher('\n');
+
+					return (0);
+					}
