c85e36e0803164ad4c5156ef791dc191cbc56a04
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-03-17 19:08:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet without q and e.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+	{
+		putchar(digital + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
