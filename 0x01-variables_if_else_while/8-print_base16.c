7eeee7273e406ec9d3f184e6da9638888967c794
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-03-17 19:23:03 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putcher(digit);
+
+		if (digit == 57)
+			digit += 39;
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
