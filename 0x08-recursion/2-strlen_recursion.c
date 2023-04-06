53aabb4e3b77bd33702c69b1a6b06c577a2d1f57
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-05 18:15:52 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function to return the lenght of the string
+ * @s:pointer string
+ * Return: int
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return(0);
+	}
+}
