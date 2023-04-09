c7b8b031b3472e914a4c99e288b97e0c7661b1b5
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-08 17:24:10 -0700

    2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-    int ch;
+int ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-    }
-    return (0);
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
 }
