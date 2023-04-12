e1bad62947637fcc318db730a7a85cc2ce938996
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-11 17:02:00 -0700

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,18 +2,20 @@
 #include <stdlib.h>
 
 /**
- * create array -  creates an array of charts
+ * create_array -  creates an array of charts and initialiazes
+ * it with a specific char
  * @size: size of an array
  * @c: char to initialized
+ * Return: pointer
 */
 
 char *create_array(unsigned int size, char c)
 {
 	char *n = malloc(size);
 
-	if (size == 0|| n == 0)
+	if (size == 0 || n == 0)
 		return (0);
-	
+
 	while (size--)
 		n[size] = c;
 
