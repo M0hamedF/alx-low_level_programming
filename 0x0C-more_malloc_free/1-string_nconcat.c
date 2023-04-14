07c91f6fd3cecda4cd32a64f123d7063a897c558
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-13 19:03:41 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,3 +8,33 @@
  * @n: number of bytes
  * Return: char
 */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+        unsigned int i, j, x, y;
+        char *str;
+
+        if (s1 == NULL)
+                s1 = nul;
+        if (s2 == NULL)
+                s2 = nul;
+
+        for (x = 0; s1[x] != '\0'; x++)
+                ;
+        for (y = 0; s2[y] != '\0'; y++)
+                ;
+        str = malloc(x + n + 1)
+                if (str == NULL)
+{
+                return (NULL);
+}
+        for (i = 0; si[i] != '\0'; i++)
+str[i] = s1[i];
+for (j = 0; j < n; j++)
+{
+str[i] = s2[j];
+i++;
+}
+str[i] = '\0'
+return (str);
+}
