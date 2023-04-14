13ea558b087ea35665c1392288b8ddc1ba7051ea
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-13 18:33:13 -0700

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,30 +11,38 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, x, y;
 	char *str;
+	char *nul = "";
+	unsigned int x, y;
 
+	x = y = 0;
 	if (s1 == NULL)
 		s1 = nul;
 	if (s2 == NULL)
 		s2 = nul;
-
-	for (x = 0; s1[x] != '\0'; x++)
-		;
-	for (y = 0; s2[y] != '\0'; y++)
-		;
-	str = malloc(x + n + 1)
-		if (str == NULL)
-{
+	while (s1[x] != '\0')
+		x += 1;
+	while (s2[y] != '\0')
+		y += 1;
+	y++;
+	if (n >= y)
+		n = y;
+	str = malloc(sizeof(*str) * n + (x + 1));
+	if (str == NULL)
 		return (NULL);
-}
-	for (i = 0; si[i] != '\0'; i++)
-str[i] = s1[i];
-for (j = 0; j < n; j++)
-{
-str[i] = s2[j];
-i++;
-}
-str[i] = '\0'
-return (str);
+	x = 0;
+	while (s1[x] != '\0')
+	{
+	str[x] = s1[x];
+	x += 1;
+	}
+	y = 0;
+	while (s2[y] != '\0' && y < n)
+	{
+	str[x] = s2[y];
+	x++;
+	y++;
+	}
+	str[x] = '\0';
+	return (str);
 }
