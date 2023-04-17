9d274b89c0190f31f707b7df63cffedba6b843e0
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-17 15:45:33 -0700

    file_name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main -a program that prints the name of the file it was compiled from
+ * main - a program that prints the name of the file it was compiled from
  * Return: 0
 */
 
 int main()
 {
 printf(" %s\n", _FILE_);
-return 0;
+return (0);
 }
