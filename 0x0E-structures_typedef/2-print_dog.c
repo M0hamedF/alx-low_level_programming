282d9e826b769a596f24e5a48fabee2dc65d3e41
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-17 17:09:03 -0700

    3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,10 @@
 
 void print_dog(struct dog *d)
 {
-if (d = NULL)
+if (d == NULL)
 {
-printf("Name: %s\n", d->name ? d->name : (nil)\n");
+printf("Name: %s\n", d->name ? d->name : "(nil)\n");
 printf("Age: %f\n", d->age);
-printf("Owner: %s\n", d->owner ? d->owner :(nil)\n");
+printf("Owner: %s\n", d->owner ? d->owner :"(nil)\n");
 }
 }
