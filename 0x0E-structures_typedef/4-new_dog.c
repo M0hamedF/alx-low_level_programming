6072dd95c5e3b8c1497d952af53da447b7f5b243
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-17 18:59:55 -0700

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - returns the lenght of a string
+ * @s: pointer to s
+ * Return: 0 on success
+ */
+int _strlen(char *s)
+{
+int count = 0;
+
+if (s != '\0')
+{
+while (*(s + count) != '\0')
+count++;
+}
+return (count);
+}
