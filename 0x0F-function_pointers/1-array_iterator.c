913adcf689c74e7846d39b2af63b026e42e73f99
Mohamed Fathy <mohamedfathy.gb@gmail.com>
2023-04-19 18:33:12 -0700

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (array != 0 && size >= 1 && action != 0)
+	if (array && size && action)
 		return;
 	for (i = 0; i < size; i++)
 		(*action)(array[i]);
